0fbf4ba19de3920468942ce0558028682a46451f
John Villa <johnvilla.daniel@gmail.com>
2024-05-15 13:59:34 -0600

    Hola

---
+++ Principal/Prinipal.java
@@ -0,0 +1,8 @@
+package Principal;
+public class Prinipal
+{
+    public static void main(String[] args) {
+        System.out.println("HOLa Mundo");
+        System.out.println("HOLa Mundo");
+    }
+}
